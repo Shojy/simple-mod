f2f84f8eac1a8f1a7d856483dd1a9e470911e404
Joshua Moon <joshua@moon.uk.net>
2015-12-17 21:55:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Mod
+
+Just a simple mod created while learning the basics of MineCraft modding.
---
+++ src/main/java/net/uk/moon/simplemod/SimpleMod.java
@@ -0,0 +1,20 @@
+package net.uk.moon.simplemod;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SimpleMod.MODID, version = SimpleMod.VERSION)
+public class SimpleMod
+{
+    public static final String MODID = "simplemod";
+    public static final String VERSION = "0.1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
